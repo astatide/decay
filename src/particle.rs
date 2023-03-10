ad08d6bdb905170681022538ebd0953846a6c28b
Astatide <aujadeva@gmail.com>
2023-03-09 21:56:44 -0500

    Playing around with traits, yadda.

--- src/particle.rs
+++ src/particle.rs
@@ -1,10 +1,43 @@
 use std::collections::HashMap;
+use uuid::Uuid;
 
 #[derive(Debug)]
 struct Particle {
-    mass: u32,
-    charge: u32,
+    mass: f32,
+    charge: f32,
+    position: Vec<f64>,
+    velocity: Vec<f64>
 }
+
+impl Particle {
+    fn new() -> Self {
+        Self {
+            mass: 0.0,
+            charge: 0.0,
+            position: Vec::new(),
+            velocity: Vec::new()
+        }
+    }
+}
+
+impl HasPhysics for Particle {
+    fn calculate_forces(&self) {
+
+    }
+}
+
+impl HasMass for Particle {
+    fn set_mass(&mut self, mass: f32) {
+        self.mass = mass;
+    }
+}
+
+impl HasCharge for Particle {
+    fn set_charge(&mut self, charge: f32) {
+        self.charge = charge;
+    }
+}
+
 
 // // these aren't actually methods/functions I care about, they're just placeholder examples.
 // impl Particle {
@@ -20,12 +53,21 @@ struct Particle {
 //     }
 // }
 
+pub trait HasMass {
+    fn set_mass(&mut self, mass: f32);
+}
+
+pub trait HasCharge {
+    fn set_charge(&mut self, charge: f32);
+}
+
 pub trait HasPhysics {
     fn calculate_forces(&self);
 }
 
 pub trait IsSpatial {
     fn generate_spatial_coordinates(nDim: u32);
+    fn set_position(pos: Vec<f64>);
 }
 
 pub trait Bonded {
@@ -48,6 +90,23 @@ struct Atom {
     neighbors: Vec<String>
 }
 
+impl Atom {
+    fn new(element: Elements) -> Self {
+        Self {
+            particle: Particle::new(),
+            element: element,
+            id: Uuid::new_v4().to_string(),
+            neighbors: Vec::new()
+        }
+    }
+}
+
+impl HasPhysics for Atom {
+    fn calculate_forces(&self) {
+        self.particle.calculate_forces();
+    }
+}
+
 // needs to implement Bondable
 struct Molecule {
     atoms: Vec<String>,
@@ -66,3 +125,24 @@ enum Elements {
     O,
     X
 }
+
+// stolen from Legion, which I also wrote so that's fine.
+/*
+class integrator : functionBase {
+  var dt: real;
+  proc this(ref atom: Particles.Atom, acc: LinAlg.vector) {
+    // leapfrog / verlet
+    atom.pos += (atom.vel*dt) + (0.5*acc*dt**2);
+    atom.vel += (acc*dt*0.5);
+  }
+  // uggghhhhh _apparently_ if we don't use this, it calls the superclass method, regardless of the arguments.  Blagh.
+  //proc this(ref atom: Particles.Atom, acc: LinAlg.vector) { this.__internal__(atom, acc); }
+}
+
+class dampingForce : functionBase {
+  var dampingStrength: real = 0.5;
+  proc this(ref atom: Particles.Atom) {
+    // bullshit damping force.
+    atom.vel *= dampingStrength;
+  }
+} */
