5625189e79f661d560d67886aa396b61a3a41427
Astatide <aujadeva@gmail.com>
2023-03-11 00:04:48 -0500

    Lots of wgpu integration

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // use crate::particle::ForceField;
 use crate::ff::ForceField; // have to add and use the trait!
+use decay::run;
 
 mod particle;
 mod ff;
 mod integrator;
 
 fn main() {
+    pollster::block_on(decay::run());
     println!("Hello, world!");
     let _sin = ff::SIN { description: "SIN".to_string() };
     println!("{:?}", _sin.atom(ff::Elements::H(0)));
