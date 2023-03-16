92c3ad9feaad12ab2e0d0c66b7752cffeb12d86e
Astatide <aujadeva@gmail.com>
2023-03-15 21:12:53 -0400

    Adding in more...

---
+++ src/gin/primitives.rs
@@ -0,0 +1,15 @@
+use super::vertex;
+
+pub const VERTICES: &[vertex::Vertex] = &[
+    vertex::Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    vertex::Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    vertex::Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    vertex::Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    vertex::Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+pub const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
