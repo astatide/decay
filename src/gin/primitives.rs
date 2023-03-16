2eb891673bc4d0fb3c00431865d98ac06b1f8a33
Astatide <aujadeva@gmail.com>
2023-03-16 13:42:49 -0400

    Fun with learning that apparently draw order in the vertex index buffer matters.

--- src/gin/primitives.rs
+++ src/gin/primitives.rs
@@ -1,15 +1,61 @@
 use super::vertex;
 
+// 1 / sqRt 3: 0.57735026919
+// 2 / sqRt 3: 1.15470053838
+// 4 / sqRt 6: 1.63299316186
+
+// 1 / sqrt(2): 0.70710678118
+// / 2 = 0.35355339059
+
+// Tetrahedron coordinates: (±1, 0, -1/√2), (0, ±1, 1/√2)
+
+const SF: f32 = 2.0; 
+
 pub const VERTICES: &[vertex::Vertex] = &[
-    vertex::Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
-    vertex::Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
-    vertex::Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
-    vertex::Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
-    vertex::Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+    vertex::Vertex { position: [-0.5/SF, 0.0, -0.35355339/SF], color: [0.5, 0.0, 0.5] }, // A
+    vertex::Vertex { position: [0.5/SF, 0.0, -0.35355339/SF], color: [0.5, 0.0, 0.5] }, // B
+    vertex::Vertex { position: [0.0, -0.5/SF, 0.35355339/SF], color: [0.5, 0.0, 0.5] }, // C
+    vertex::Vertex { position: [0.0, 0.5/SF, 0.35355339/SF], color: [0.5, 0.0, 0.5] } // D
 ];
 
+// FUCK!  Apparently order DOES matter.  WHAT?!
 pub const INDICES: &[u16] = &[
-    0, 1, 4,
-    // 1, 2, 4,
-    // 2, 3, 4,
+    1, 3, 2, // F 1
+    3, 1, 0, // F 2
+    2, 0, 1, // F 3
+    0, 2, 3, // F 4
+    // 0, 1, 2, // F3 but not
+    // 0, 1, 3, // F 2 but not
+    // 0, 2, 3, // F 4
+    // 1, 2, 3 // F 1 but not
 ];
+
+// we need to draw a set of coordinates for each face.
+// wait, how the fuck does this magnet shit work.
+// alright, I exhaustively did everything and it works.  Need to go back and read what I'm missing here re: whether... does order matter?  That's weird.
+// pub const INDICES: &[u16] = &[
+//     0, 1, 2,
+//     0, 2, 1,
+//     0, 1, 3,
+//     0, 3, 1,
+//     0, 3, 2,
+//     0, 2, 3,
+//     1, 2, 3,
+//     1, 3, 2,
+//     1, 0, 2,
+//     1, 2, 0,
+//     1, 3, 0,
+//     1, 0, 3,
+//     2, 0, 1,
+//     2, 1, 0,
+//     2, 0, 3,
+//     2, 3, 0,
+//     2, 1, 3,
+//     2, 3, 1,
+//     3, 2, 1,
+//     3, 1, 2,
+//     3, 0, 1,
+//     3, 1, 0,
+//     3, 0, 2,
+//     3, 2, 0
+// ];
