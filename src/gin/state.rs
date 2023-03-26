c2540a470e788fbc5b00eb1980068098a2ae47d3
Astatide <aujadeva@gmail.com>
2023-03-26 14:39:41 -0400

    Ah, may as well update some of the other structures, too.

--- src/gin/state.rs
+++ src/gin/state.rs
@@ -128,7 +128,7 @@ impl State {
         // load in the shaders!
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("../shader.wgsl").into()),
+            source: wgpu::ShaderSource::Wgsl(include_str!("../shaders/shader.wgsl").into()),
         });
 
         // camera render stuff
